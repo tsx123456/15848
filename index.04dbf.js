3620da0c7cc30ddf4dedffe782774804ac47bc4b
上帝的梦 <1597691532@qq.com>
2025-10-15 17:41:17 +0800

    1

--- index.04dbf.js
+++ index.04dbf.js
@@ -1,4 +1,4 @@
-System.register(["./application.8f2ef.js"], function (_export, _context) {
+System.register(["./application.54e0a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
