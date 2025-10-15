a0b23806be5849f5230d76d99de627fc72301770
上帝的梦 <1597691532@qq.com>
2025-10-15 17:16:44 +0800

    1

--- cocos-js/meshopt_decoder.wasm-43855155.js
+++ cocos-js/meshopt_decoder.wasm-43855155.js
@@ -1,10 +1,1 @@
-System.register([], (function (exports, module) {
-	'use strict';
-	return {
-		execute: (function () {
-
-			var meshopt_decoder_wasm = exports('default', 'assets/meshopt_decoder.wasm-12c0a404.wasm'); /* asset-hash:bedd883e */
-
-		})
-	};
-}));
+System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/meshopt_decoder.wasm-12c0a404.wasm")}}}));
